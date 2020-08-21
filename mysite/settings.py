5d02f56c472b68f5ebc4bb2a558166b5afab7eb9
hiroshi <s20840011@gmail.com>
2020-08-21 21:36:00 +0900

    お問い合わせ作成

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,15 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# 開発用
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# 本番用
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 's20840011@gmail.com'
+# EMAIL_HOST_PASSWORD = 'ss8632'
+# EMAIL_USE_TLS = True
